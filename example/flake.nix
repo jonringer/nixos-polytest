00661bd6b2b7f59398b55207585eb961ec42e680
Jonathan Ringer <jonringer117@gmail.com>
2024-06-17 19:12:56 -0700

    Add example

--- example/flake.nix
+++ example/flake.nix
@@ -4,20 +4,37 @@
   inputs = {
     nixpkgs-2311.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-2405.url = "github:nixos/nixpkgs/nixos-24.05";
-    poly-test.url = "github:jonringer/nixos-polytest?ref=master";
+    #poly-test.url = "github:jonringer/nixos-polytest";
+    poly-test.url = "path:///home/jringer/projects/poly-runtest";
   };
 
-  outputs = { nixpkgs-2311, nixpkgs-2405, poly-test, self }: {
-    checks = poly-test.lib.runPolyTest {
+  outputs = { nixpkgs-2311, nixpkgs-2405, poly-test, self }: let
+    hostPkgs = import nixpkgs-2311 { system = "x86_64-linux"; };
+    runPolyTest = poly-test.lib.mkRunPolyTest hostPkgs;
+  in {
+    checks.x86_64-linux.example = runPolyTest {
+      name = "example-polytest";
+      testScript = ''
+        start_all()
+        foo.wait_for_unit("multi-user.target")
+        bar.wait_for_unit("multi-user.target")
+
+        foo.succeed("cat /etc/os-release")
+        bar.succeed("cat /etc/os-release")
+      '';
       nodes.a = {
         nixpkgsPath = toString nixpkgs-2311;
         specialArgs = { };
-        modules = [ ];
+        modules = [ {
+          system.name = "foo";
+        }];
       };
       nodes.b = {
         nixpkgsPath = toString nixpkgs-2405;
         specialArgs = { };
-        modules = [ ];
+        modules = [ {
+          system.name = "bar";
+        }];
       };
     };
   };
